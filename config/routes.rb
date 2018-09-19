3eaf7bd960baf260798e1628206efeabdd0e6a08
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-19 22:23:52 +0900

    create Session_controller,Home_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/login'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'home#index'
   get '/auth/twitter/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
