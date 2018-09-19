a073cb584762675fea4909cab9f8a41ac3caa82e
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-19 22:39:47 +0900

    controllerの処理を書きました

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if session[:user_id].nil?
+      redirect_to action:'login'
+    else
+      @user = User.find(session[:user_id])
+    end
   end
 
   def login
