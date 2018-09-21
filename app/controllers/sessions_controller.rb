83fbe2781d412b69e5317e4dcfbf680146d786f8
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-21 11:08:15 +0900

    oauth_keyを持ってこれるようにしたつもり

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,9 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+
     session[:user_id] = user.id
+    session[:oauth_token] = user.
     redirect_to root_url
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,25 @@
 class User < ApplicationRecord
   def self.find_or_create_from_auth(auth)
+    # ここ何やってるか全くわかんねぇ。
+    # Oauth取得 → セッション保存 → 書き換え みたいなことやってるの?(
     provider = auth[:provider]
     uid = auth[:uid]
     nickname = auth[:info][:nickname]
     name = auth[:info][:name]
     image_url = auth[:info][:image]
     description = auth[:info][:description]
+
+    # わからんけどアクセストークン欲しいから貰えそうなコード追記
+    oauth_token = auth[:credentials][:token]
+    oauth_token_secret = auth[:credentials][:secret]
+
     self.find_or_create_by(provider: provider, uid: uid) do |user|
       user.nickname = nickname
       user.name = name
       user.image_url = image_url
       user.description = description
+      user.oauth_token = oauth_token
+      user.oauth_token_secret = oauth_token_secret
     end
   end
 end
