b2fa27f5073792c247f41951b0064bac5e67a13a
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-23 06:02:44 +0900

    text,dateを確実に持ってくるように修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,49 +13,64 @@ class HomeController < ApplicationController
 
 
   def tweet
-    @user = User.find(session[:user_id])
-    @client = twitter_client
+    if session[:user_id].nil?
+      redirect_to action: 'login'
+    else
+      @user = User.find(session[:user_id])
+      @client = twitter_client
 
-    if session[:tweet_items].nil?
+      # sessionにツイートデータが無ければ、ツイート取得クローラ走らせてちょびちょびsessionに保存
       if session[:tweet_items].nil?
-        session[:tweet_items] = Array.new
-      end
-      @search_count = @client.user.tweets_count / 20
+        if session[:tweet_items].nil?
+          session[:tweet_items] = Array.new
+        end
+        @search_count = @client.user.tweets_count / 20
 
-      @first_twi_id = @client.user_timeline.first.id
+        @first_twi_id = @client.user_timeline.first.id
 
-      @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@first_twi_id}&reset_error_state=false")
-      @twi_result = JSON.parse(Net::HTTP.get(@uri))
-
-      @twi_items = @twi_result["items_html"]
-      @twi_ids = @twi_items.scan(/data-tweet-id="(.+)"/)
-      @twi_ids.flatten!
-
-      @search_count.times do |i|
-        @twi_id_to_url = @twi_ids.last
-        @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@twi_id_to_url}&reset_error_state=false")
+        @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@first_twi_id}&reset_error_state=false")
         @twi_result = JSON.parse(Net::HTTP.get(@uri))
+
         @twi_items = @twi_result["items_html"]
-        @twi_ids.push(@twi_items.scan(/data-tweet-id="(.+)"/))
+        @twi_ids = @twi_items.scan(/data-tweet-id="(.+)"/)
         @twi_ids.flatten!
-        session[:tweet_items].push(@twi_ids.sample)
-        sleep(0.05)
-        if i >= 80
+
+        @search_count.times do |i|
+          @twi_id_to_url = @twi_ids.last
+          @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@twi_id_to_url}&reset_error_state=false")
+          @twi_result = JSON.parse(Net::HTTP.get(@uri))
+          @twi_items = @twi_result["items_html"]
+          @twi_ids.push(@twi_items.scan(/data-tweet-id="(.+)"/))
+          @twi_ids.flatten!
+          session[:tweet_items].push(@twi_ids.sample)
+          sleep(0.03)
+          if i >= 100
+            break
+          end
+        end
+      end
+
+      # テキストと時刻がちゃんと取得できるまでIDを回す
+      loop do
+        @tweet_uri = Net::HTTP.get(URI.parse("https://twitter.com/#{@user[:nickname]}/status/#{session[:tweet_items].sample}"))
+        @tweet_text = @tweet_uri.force_encoding("UTF-8").scan(/<p class="TweetTextSize TweetTextSize--jumbo js-tweet-text tweet-text".+>(.+)<\/p>/)
+        @tweet_date = @tweet_uri.force_encoding("UTF-8").scan(/<span>.*(\d+年\d+月\d+日)<\/span>/)
+        unless @tweet_text.class.nil? && @tweet_date.class.nil?
           break
         end
       end
+
+      # 配列やダブルクオーテーションを処理
+      @tweet_text = CGI.unescapeHTML(@tweet_text[0][0])
+      @tweet_date = @tweet_date[0][0]
+
+      # スクレイピングの都合で発生した各タグを処理。正規表現抜きで処理できるならしたい。
+      @tweet_text_link_excluded = @tweet_text.gsub(/<.*?>/, "").gsub(/<\/a>/, "")
+
+      # 文字数制限。
+      @update_tweet_text = @tweet_text_link_excluded.truncate(120, omission: '...')
+      @update_text = "#{@update_tweet_text}\r\n#{@tweet_date}"
     end
-
-    @tweet_uri = Net::HTTP.get(URI.parse("https://twitter.com/#{@user[:nickname]}/status/#{session[:tweet_items].sample}"))
-    @tweet_text = @tweet_uri.force_encoding("UTF-8").scan(/<p class="TweetTextSize TweetTextSize--jumbo js-tweet-text tweet-text" lang="ja" data-aria-label-part="0">(.+)<\/p>/)[0][0]
-    @tweet_date = @tweet_uri.force_encoding("UTF-8").scan(/<span>.*(\d{4}年\d{1,2}月\d{1,2}日)<\/span>/)[0][0]
-
-    @tweet_text = CGI.unescapeHTML(@tweet_text)
-    @tweet_text_link_excluded = @tweet_text.gsub(/<.*?>/,"").gsub(/<\/a>/,"")
-
-
-    @update_tweet_text = @tweet_text_link_excluded.truncate(110, omission: '...')
-    @update_text = "#{@update_tweet_text}　\r\n#{@tweet_date}"
   end
 
 
