bdd79915a27639fbbe040a519ef18627ba64758d
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-23 06:53:53 +0900

    しょーもないtypo修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -62,7 +62,7 @@ class HomeController < ApplicationController
 
       # 配列やダブルクオーテーションを処理
       @tweet_text = CGI.unescapeHTML(@tweet_text[0][0])
-      @tweet_date_text = @tweet_date[0][0]
+      @tweet_date_text = @tweet_date
 
       # スクレイピングの都合で発生した各タグを処理。正規表現抜きで処理できるならしたい。
       @tweet_text_link_excluded = @tweet_text.gsub(/<.*?>/, "").gsub(/<\/a>/, "")
