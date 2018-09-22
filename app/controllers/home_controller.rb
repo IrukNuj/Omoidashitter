0d1189c8f95072985010b7c3cfafa8670f8d09e2
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-23 07:05:48 +0900

    インスタンスにはenptyのが良さげ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -55,14 +55,16 @@ class HomeController < ApplicationController
         @tweet_uri = Net::HTTP.get(URI.parse("https://twitter.com/#{@user[:nickname]}/status/#{session[:tweet_items].sample}"))
         @tweet_text = @tweet_uri.force_encoding("UTF-8").scan(/<p class="TweetTextSize TweetTextSize--jumbo js-tweet-text tweet-text".+>(.+)<\/p>/)
         @tweet_date = @tweet_uri.force_encoding("UTF-8").scan(/<span>.*(\d{4}年\d{1,2}月\d{1,2}日)<\/span>/)
-        unless @tweet_text.nil? || @tweet_date.nil?
-          break
+        unless @tweet_text.empty?
+          unless @tweet_date.empty?
+            break
+          end
         end
       end
 
       # 配列やダブルクオーテーションを処理
       @tweet_text = CGI.unescapeHTML(@tweet_text[0][0])
-      @tweet_date_text = @tweet_date
+      @tweet_date_text = @tweet_date[0][0]
 
       # スクレイピングの都合で発生した各タグを処理。正規表現抜きで処理できるならしたい。
       @tweet_text_link_excluded = @tweet_text.gsub(/<.*?>/, "").gsub(/<\/a>/, "")
