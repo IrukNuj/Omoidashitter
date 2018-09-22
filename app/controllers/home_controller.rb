43196d83d05c32ea74fd82b619d173c917237bb7
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-22 23:51:36 +0900

    ツイートクローラーのコーディング

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,58 +7,38 @@ class HomeController < ApplicationController
     else
       @user = User.find(session[:user_id])
       @client = twitter_client
-
-      @search_url = URI.escape("https://twitter.com/#{@user.nickname}")
-
-      @first_twi_id = @client.user_timeline.first.id
-
-      uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@first_twi_id}&reset_error_state=false")
-      @twi_json = Net::HTTP.get(uri)
-      @twi_result = JSON.parse(@twi_json)
-
-      # @oembed_tweets = Array.new
-      # @doc.css('p,js-tweet-text').each_with_index do |li, index|
-      #   tweet_id = li['data-item-id']
-      #
-      #   if tweet_id
-      #     @oembed_tweets.push(@twitter_client.oembed(tweet_id).html)
-      #   end
-      # end
-
-      # @client_timeline_last = @client.user_timeline.first
-      # @client_timeline = (@client.user_timeline({max_id: @client_timeline_last.id, count: 200,exclude_replies: true}))
-      # tweet全取得
-      # @client_timeline = Array.new
-      # search_count = (@client.user.tweets_count / 200) + 1
-      #
-      # @client_timeline_last = @client.user_timeline.first
-      #
-      # search_count.times do |i|
-      #   @client_timeline.push(@client.user_timeline({max_id: @client_timeline_last.id, count: 200}))
-      #   @client_timeline_last = @client_timeline.last.last
-      # end
-
     end
   end
 
   def tweet
-
+    @user = User.find(session[:user_id])
     @client = twitter_client
-    @client_timeline = Array.new
-    search_count = @client.user.tweets_count / 200
 
-    @client_timeline_last = @client.user_timeline.first
+    @search_count = @client.user.tweets_count / 20
 
-    search_count.times do |i|
-      @client_timeline.push(@client.user_timeline({max_id: @client_timeline_last.id, count: 200}))
-      @client_timeline_last = @client_timeline.last.last
-      if i  >= 10
+    @first_twi_id = @client.user_timeline.first.id
+
+    @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@first_twi_id}&reset_error_state=false")
+    @twi_result = JSON.parse(Net::HTTP.get(@uri))
+
+    @twi_items = @twi_result["items_html"]
+    @twi_ids = @twi_items.scan(/data-tweet-id="(.+)"/)
+    @twi_ids.flatten!
+
+    @search_count.times do |i|
+      @twi_id_to_url = @twi_ids.last
+      @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@twi_id_to_url}&reset_error_state=false")
+      @twi_result = JSON.parse(Net::HTTP.get(@uri))
+      @twi_items = @twi_result["items_html"]
+      @twi_ids.push(@twi_items.scan(/data-tweet-id="(.+)"/))
+      @twi_ids.flatten!
+      sleep(0.1)
+      if i >= 80
         break
       end
     end
 
-    tweet_text = @client_timeline.last.sample
-    # tweet_date = @client_timeline.sample.sample
+    tweet_text = @client.status(@twi_ids.sample)
 
     @client.update!("#{tweet_text.text} \n#{tweet_text.created_at.strftime("%Y/%m/%d")}　#おもいだしったー")
 
@@ -75,8 +55,8 @@ class HomeController < ApplicationController
     Twitter::REST::Client.new do |config|
       config.consumer_key = Rails.application.secrets.twitter_api_key
       config.consumer_secret = Rails.application.secrets.twitter_api_secret
-      config.access_token = session[:oauth_token]
-      config.access_token_secret = session[:oauth_token_secret]
+      config.access_token = @user.oauth_token
+      config.access_token_secret = @user.oauth_token_secret
     end
   end
 end
