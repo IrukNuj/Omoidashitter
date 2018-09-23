564d2df9c940bdf9fb278ce9e1238ae0cd88403b
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-23 15:07:08 +0900

    インスタンスにはenptyのが良さげ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -47,14 +47,14 @@ class HomeController < ApplicationController
       end
 
       # テキストと時刻がちゃんと取得できるまでIDを回す
-      loop do
+      # loop do
         @tweet_uri = Net::HTTP.get(URI.parse("https://twitter.com/#{@user[:nickname]}/status/#{session[:tweet_items].sample}"))
         @tweet_text = @tweet_uri.force_encoding("UTF-8").scan(/<p class="TweetTextSize TweetTextSize--jumbo js-tweet-text tweet-text".+>(.+)<\/p>/)
         @tweet_date = @tweet_uri.force_encoding("UTF-8").scan(/<span>.*(\d{4}年\d{1,2}月\d{1,2}日)<\/span>/)
-        unless @tweet_text.empty? || @tweet_date.empty?
-          break
-        end
-      end
+      #   unless @tweet_text.empty? || @tweet_date.empty?
+      #     break
+      #   end
+      # end
 
       # 配列やダブルクオーテーションを処理
       @tweet_text = CGI.unescapeHTML(@tweet_text[0][0])
@@ -67,19 +67,20 @@ class HomeController < ApplicationController
       @update_tweet_text = @tweet_text_link_excluded.truncate(120, omission: '...')
       @update_text = "#{@update_tweet_text}\r\n#{@tweet_date_text}"
     end
+  end
 
 
-    def login
-    end
+  def login
+  end
 
-    private
+  private
 
-    def twitter_client
-      Twitter::REST::Client.new do |config|
-        config.consumer_key = Rails.application.secrets.twitter_api_key
-        config.consumer_secret = Rails.application.secrets.twitter_api_secret
-        config.access_token = @user.oauth_token
-        config.access_token_secret = @user.oauth_token_secret
-      end
+  def twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.twitter_api_key
+      config.consumer_secret = Rails.application.secrets.twitter_api_secret
+      config.access_token = @user.oauth_token
+      config.access_token_secret = @user.oauth_token_secret
     end
   end
+end
