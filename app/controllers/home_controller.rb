c60f1537892cc90fd047cbe2582874996cc35205
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-23 15:01:02 +0900

    インスタンスにはenptyのが良さげ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -55,22 +55,21 @@ class HomeController < ApplicationController
         @tweet_text = @tweet_uri.force_encoding("UTF-8").scan(/<p class="TweetTextSize TweetTextSize--jumbo js-tweet-text tweet-text".+>(.+)<\/p>/)
         @tweet_date = @tweet_uri.force_encoding("UTF-8").scan(/<span>.*(\d{4}年\d{1,2}月\d{1,2}日)<\/span>/)
         unless @tweet_text.empty? || @tweet_date.empty?
-            break
-          end
+          break
         end
       end
-
-      # 配列やダブルクオーテーションを処理
-      @tweet_text = CGI.unescapeHTML(@tweet_text[0][0])
-      @tweet_date_text = @tweet_date[0][0]
-
-      # スクレイピングの都合で発生した各タグを処理。正規表現抜きで処理できるならしたい。
-      @tweet_text_link_excluded = @tweet_text.gsub(/<.*?>/, "").gsub(/<\/a>/, "")
-
-      # 文字数制限。
-      @update_tweet_text = @tweet_text_link_excluded.truncate(120, omission: '...')
-      @update_text = "#{@update_tweet_text}\r\n#{@tweet_date_text}"
     end
+
+    # 配列やダブルクオーテーションを処理
+    @tweet_text = CGI.unescapeHTML(@tweet_text[0][0])
+    @tweet_date_text = @tweet_date[0][0]
+
+    # スクレイピングの都合で発生した各タグを処理。正規表現抜きで処理できるならしたい。
+    @tweet_text_link_excluded = @tweet_text.gsub(/<.*?>/, "").gsub(/<\/a>/, "")
+
+    # 文字数制限。
+    @update_tweet_text = @tweet_text_link_excluded.truncate(120, omission: '...')
+    @update_text = "#{@update_tweet_text}\r\n#{@tweet_date_text}"
   end
 
 
