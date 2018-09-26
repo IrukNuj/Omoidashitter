7cf71f752bd3b2d1e39edc205ff6939657612f1d
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-09-26 15:54:40 +0900

    before install bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,33 +9,23 @@ class HomeController < ApplicationController
       @user = User.find(session[:user_id])
       @client = twitter_client
 
-      # sessionにツイートデータが無ければ、ツイート取得クローラ走らせてちょびちょびsessionに保存
+      @first_twi_id = @client.user_timeline.first.id
 
-      if session[:tweet_items].nil?
-        if session[:tweet_items].nil?
-          session[:tweet_items] = Array.new
-        end
-        @search_count = @client.user.tweets_count / 200
 
-        @first_twi_id = @client.user_timeline.first.id
-
-        @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@first_twi_id}&reset_error_state=false")
-        @twi_result = JSON.parse(Net::HTTP.get(@uri))
-
-        @twi_items = @twi_result["items_html"]
-        @twi_ids = @twi_items.scan(/data-tweet-id="(.+)"/)
-        @twi_ids.flatten!
-
-        @search_count.times do |i|
-          @twi_id_to_url = @twi_ids.last
-          @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@twi_id_to_url}&reset_error_state=false")
-          @twi_result = JSON.parse(Net::HTTP.get(@uri))
-          @twi_items = @twi_result["items_html"]
-          @twi_ids.push(@twi_items.scan(/data-tweet-id="(.+)"/))
-          @twi_ids.flatten!
-          session[:tweet_items].push(@twi_ids.sample)
-        end
-      end
+      # @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@first_twi_id}&reset_error_state=false")
+      # @twi_result = JSON.parse(Net::HTTP.get(@uri))
+      #
+      #
+      # url = @twi_result["items_html"]
+      #
+      # charset = nil
+      # html = url.each do |f|
+      #   charset = f.charset # 文字種別を取得
+      #   f.read # htmlを読み込んで変数htmlに渡す
+      # end
+      #
+      # @doc = Nokogiri::HTML.parse(html, nil, charset)
+      # @doc = @doc.xpath('//a')[0]
 
     end
   end
@@ -47,6 +37,39 @@ class HomeController < ApplicationController
     else
       @user = User.find(session[:user_id])
       @client = twitter_client
+
+      # sessionにツイートデータが無ければ、ツイート取得クローラ走らせてちょびちょびsessionに保存
+      if session[:tweet_items].nil?
+        if session[:tweet_items].nil?
+          session[:tweet_items] = Array.new
+        end
+        @search_count = @client.user.tweets_count / 20
+        @first_twi_id = @client.user_timeline.first.id
+
+        @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@first_twi_id}&reset_error_state=false")
+        @twi_result = JSON.parse(Net::HTTP.get(@uri))
+
+        @twi_items = @twi_result["items_html"]
+        @twi_ids = @twi_items.scan(/data-tweet-id="(.+)"/)
+        @twi_ids.flatten!
+
+        @search_count.times do |i|
+          @uri = URI.parse("https://twitter.com/i/profiles/show/#{@user.nickname}/timeline/tweets?include_available_features=1&include_entities=1&max_position=#{@twi_ids.last}&reset_error_state=false")
+          @twi_result = JSON.parse(Net::HTTP.get(@uri))
+          @twi_items = @twi_result["items_html"]
+          @twi_ids.push(@twi_items.scan(/data-tweet-id="(.+)"/))
+          @twi_ids.flatten!
+          session[:tweet_items].push(@twi_ids.sample)
+
+          # 実装時には死んでも消さないこと
+          sleep(1)
+          #
+
+          if i >= 50
+            break
+          end
+        end
+      end
 
       # テキストと時刻がちゃんと取得できるまでIDを回す
       loop do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     session[:user_id] = user.id
+    # ToDo 直す　絶対やること
     session[:oauth_token] = user.oauth_token
     session[:oauth_token_secret] = user.oauth_token_secret
 
