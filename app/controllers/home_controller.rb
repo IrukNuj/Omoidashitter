b0c41d6864ca8aa899bb648651e6c8ec336e6b2c
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-10-05 12:15:36 +0900

    ツイート取得アルゴリズムの修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,34 +24,29 @@ class HomeController < ApplicationController
 
       # テキストと時刻がちゃんと取得できるまでIDを回す
       loop do
-        tweet_uri = Net::HTTP.get(URI.parse("https://twitter.com/#{@user[:nickname]}/status/#{session[:tweet_items].sample}"))
+        tweet_id = session[:tweet_items].sample
+        tweet_uri = Net::HTTP.get(URI.parse("https://twitter.com/#{@user[:nickname]}/status/#{tweet_id}"))
         puts tweet_uri.force_encoding("UTF-8").scan(/<span>.*(\d{4}年\d{1,2}月\d{1,2}日)<\/span>/)
         @tweet_text = tweet_uri.force_encoding("UTF-8").scan(/<p class="TweetTextSize TweetTextSize--jumbo js-tweet-text tweet-text".+>(.+)<\/p>/)
-        puts @tweet_text
+        @tweet_date = Time.at(((tweet_id.to_i >> 22) + 1288834974675) / 1000.0).strftime("%Y年%m月%d日")
         unless @tweet_text.empty?
           puts @tweet_text
           puts "--------------------------------------------------------------------"
-          puts tweet_uri.force_encoding("UTF-8").scan(/<span>.*(\d{4}年\d{1,2}月\d{1,2}日)<\/span>/)
-
-          @tweet_date = tweet_uri.force_encoding("UTF-8").scan(/<span>.*(\d{4}年\d{1,2}月\d{1,2}日)<\/span>/)
           puts @tweet_date
-          unless @tweet_date.empty?
-            break
-          end
+          break
         end
       end
 
 
       # 配列やダブルクオーテーションを処理
       @tweet_text = CGI.unescapeHTML(@tweet_text[0][0])
-      @tweet_date_text = @tweet_date[0][0]
 
       # スクレイピングの都合で発生した各タグを処理。正規表現抜きで処理できるならしたい。
       @tweet_text_link_excluded = @tweet_text.gsub(/<.*?>/, "").gsub(/<\/a>/, "")
 
       # 文字数制限。
       @update_tweet_text = @tweet_text_link_excluded.truncate(120, omission: '...')
-      @update_text = "#{@update_tweet_text} \r\n#{@tweet_date_text}"
+      @update_text = "#{@update_tweet_text} \r\n#{@tweet_date}"
     end
   end
 
