4e43c9e4c79b2a01842a726b275ff7222d4ea2ed
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-01-19 16:28:32 +0900

    :login :index :tweetのspecの作成

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -34,20 +34,44 @@ RSpec.describe HomeController, type: :controller do
   end
 
   describe '#tweetを受け取ったとき' do
-    before do
-      get :tweet
-    end
 
     describe 'Oauth認証していなければ' do
+      before do
+        get :tweet
+      end
       it 'リダイレクト処理を返すこと' do
         expect(response).to redirect_to(home_login_path)
+      end
+    end
+
+    context 'Oauth認証していれば' do
+      before do
+        OmniAuth.config.test_mode = true
+        OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+           'provider' => 'twitter',
+           'uid'  => '2848699063',
+           'info' => {
+             'name'  => 'いるく',
+           },
+         })
+        session[:user_id] = OmniAuth.config.mock_auth[:twitter].uid
+        get :tweet
+      end
+      it '/index に飛ばすこと' do
+        expect(response).to render_template :tweet
       end
     end
   end
 
   describe '#loginを受け取った時' do
+    before do
+      get :login
+    end
     it '200ステータスを返すこと' do
       expect(response.status).to eq 200
+    end
+    it '/login　にアクセスできること' do
+      expect(response).to render_template :login
     end
   end
 
